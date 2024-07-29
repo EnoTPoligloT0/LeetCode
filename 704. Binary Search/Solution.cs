82a94d93589630cf55cd8b083eb0727ca07d9174
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-07-29 23:34:10 +0200

    704. Binary Search

---
+++ 704. Binary Search/Solution.cs
@@ -0,0 +1,31 @@
+namespace LeetCode._704._Binary_Search;
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length -1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
